0929a1ebaa715b34f48426143a36bd97f8b13ac1
gunjanbishnoi029-dotcom <gunjanbishnoi029@gmail.com>
2026-02-11 15:15:00 +0530

    roots taken

---
+++ src/components/common/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Button = ({height,width}) => {
+  return (
+    <div>
+
+       <button className={`${primary?" bg-pink ":""} `}>
+
+       </button>
+    </div>
+  )
+}
+
+export default Button
