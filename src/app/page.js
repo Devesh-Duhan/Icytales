9a80761d45c25608ad93964808b12f83297f4772
Poonia <rohtashpoonia2274@gmail.com>
2026-02-12 10:19:29 +0530

    taking pull from new dev and common

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <p className=" text-[40px] font-[berkshire] text-black">hello</p>
+     
     </>
   );
 }
