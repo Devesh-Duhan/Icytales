e73472e08b4124c7a720e3ec7b90ab5c82dfa42a
gunjanbishnoi029-dotcom <gunjanbishnoi029@gmail.com>
2026-02-12 14:18:36 +0530

    common button

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,10 @@
-import Image from "next/image";
+import Button from "@/components/common/Button";
+
 
 export default function Home() {
-  return (
-<<<<<<< HEAD
-   <>
-   
-   </>
-=======
-    <>
-      <p className=" text-[40px] font-[berkshire] text-black">hello</p>
-    </>
->>>>>>> ab88a3c67204d954e8aeac14e7fc853225bac185
-  );
+  return (
+    <>
+     <Button  />
+    </>
+  );
 }
